50a0e7322d62cee1f967b6030274329ade3e5c6e
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-03-04 18:29:21 -0800

    Video 64. The Statement + Challenge Exercise

---
+++ CalculateInterestMethod.java
@@ -0,0 +1,11 @@
+public class CalculateInterestMethod{
+  public static void main(String[] args){
+    for (int i = 2; i < 9; i++){
+      System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+    }
+  }
+
+  public static double calculateInterest(double amount, double interestRate){
+    return (amount * (interestRate / 100));
+  }
+}
